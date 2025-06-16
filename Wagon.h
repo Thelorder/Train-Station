439b48809218066462ef9e728ce8f892b6f8bf77
Ilin Ilchev <ilin.ilchev@gmail.com>
2025-06-16 17:52:33 +0300

    adding all the necceary functionalities for the Wagon Base Class

--- Wagon.h
+++ Wagon.h
@@ -1,1 +1,45 @@
+#pragma once
+#include "String.h"
+#include "Vector.hpp"
 
+class Wagon {
+protected:
+    int id;
+    float basePrice;
+    Vector<int> seats;
+    String wagonType;
+
+    String formatSeatNumber(int num) const {
+        char buffer[3];
+        if (num < 10) {
+            buffer[0] = '0';
+            buffer[1] = '0' + num;
+        }
+        else {
+            buffer[0] = '0' + (num / 10);
+            buffer[1] = '0' + (num % 10);
+        }
+        buffer[2] = '\0';
+        return String(buffer);
+    }
+
+    virtual int getSeatsPerRow() const = 0;
+
+public:
+    Wagon(int id, float basePrice, size_t seatCount, const String& type);
+    virtual ~Wagon() = default;
+
+    bool bookSeat(int seatId);
+    bool isSeatBooked(int seatId) const;
+
+    virtual void saveToFile(const String& filename) const;
+    virtual void loadFromFile(const String& filename);
+
+    virtual float calculatePrice(float distance, const String& extraParam) const = 0;
+    void printDetails() const;
+
+    int getId() const { return id; }
+    const String& getType() const { return wagonType; }
+    size_t getSeatCount() const { return seats.get_size(); }
+    virtual String getTypeHeader() const = 0;
+};
